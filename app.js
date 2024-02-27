462c0044a79107890f6465b813230d9695800f6b
okediarthur <okediarthur@gmail.com>
2024-02-27 10:23:15 +0300

    Script Adjustment

--- app.js
+++ app.js
@@ -27,7 +27,31 @@ function Book(title, author, pages, read){
 }
 
 function addBookToLibrary(){
-    //Get book title
+    //Get values from input fields
     var title = document.getElementById('title').value;
-    
+    var author = document.getElementById('author').value;
+    var pages = document.getElementById('pages').value;
+    var read = document.getElementById('read').checked;
+
+    // Create a new book Instance
+
+    var newBook = new Book(title, author, pages, read);
+
+    //add the new book to the library
+    myLibrary.push(newBook);
+
+    //clear the input fields
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('pages').value = '';
+    document.getElementById('read').checked = false;
+
+    //update the displayed library
+    updateBookLibrary();
+}
+//Function to update the displayed library
+function updateBookLibrary(){
+    var bookList = document.getElementById('myLibrary');
+    //Clear the list
+    bookList.innerHTML = '';
 }
